2c8eb910ccf10c86d6c2879b54b91192e199cff8
Thais Evelin Barbosa da Silva <109097436+ThaisEv@users.noreply.github.com>
2024-11-12 14:04:46 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BuscaFilmeComponent } from './busca-filme/busca-filme.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BuscaFilmeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ src/app/busca-filme/busca-filme.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormControl, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-busca-filme',
+  templateUrl: './busca-filme.component.html',
+  styleUrl: './busca-filme.component.css'
+})
+export class BuscaFilmeComponent {
+  formBusca: For
+
+}
